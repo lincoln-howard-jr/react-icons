5f6efe8f32db2bb6aace58af3da6f4d19a86f5ca
Lincoln Howard Jr <lincoln.c.howard.jr@gmail.com>
2025-03-15 03:43:50 -0400

    v1.0.2

--- index.tsx
+++ index.tsx
@@ -1,21 +1,21 @@
 import { IconProps } from './icons/IconProps';
 import { genCancelIcon } from './icons/cancel';
-export {CancelIcon} from './icons/cancel';
+export { CancelIcon } from './icons/cancel';
 import { genPenIcon } from './icons/pen';
-export {PenIcon} from './icons/pen';
+export { PenIcon } from './icons/pen';
 import { genChatBubblesIcon } from './icons/chat';
-export {ChatBubbles} from './icons/chat'
+export { ChatBubbles } from './icons/chat'
 import { genSettingsIcon } from './icons/settings';
 export { SettingsIcon } from './icons/settings'
 import { genUserIcon } from './icons/user'
 import { genBookIcon } from './icons/book';
-export {UserIcon} from './icons/user'
+export { UserIcon } from './icons/user'
 
 export const createTheme = (theme: IconProps) => ({
-    cancel: genCancelIcon (theme),
-    pen: genPenIcon (theme),
-    chat: genChatBubblesIcon (theme),
-    settings: genSettingsIcon (theme),
-    user: genUserIcon (theme),
-    book: genBookIcon (theme),
+    cancel: genCancelIcon(theme),
+    pen: genPenIcon(theme),
+    chat: genChatBubblesIcon(theme),
+    settings: genSettingsIcon(theme),
+    user: genUserIcon(theme),
+    book: genBookIcon(theme),
 })
